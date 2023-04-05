e5c225176774e5a13a821440ae169b8d298496d6
Zsolt Nagy <zsolt.nagy@nodim.hu>
2023-04-05 20:06:57 +0200

    update code quality rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,23 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["svelte3", "@typescript-eslint"],
+  plugins: ["svelte3", "@typescript-eslint", "unused-imports"],
   rules: {
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "no-case-declarations": "off",
     "no-empty": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
-  ignorePatterns: ["*.cjs"],
   overrides: [
     {
       files: ["*.svelte"],
