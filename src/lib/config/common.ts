7f56e0795fa57ad9a639b838758ca7241dc837f1
Zsolt Nagy <zsolt.nagy@nodim.hu>
2023-04-15 15:37:33 +0200

    make SSL not required in dev

--- src/lib/config/common.ts
+++ src/lib/config/common.ts
@@ -1,2 +1,8 @@
+import { dev } from "$app/environment";
+
 export const TYCHE_USER_JWT_HEADER_NAME = "Tyche-User-JWT";
-export const TYCHE_USER_JWT_COOKIE_NAME = `__Host-${TYCHE_USER_JWT_HEADER_NAME}`;
+export let TYCHE_USER_JWT_COOKIE_NAME = TYCHE_USER_JWT_HEADER_NAME;
+
+if (!dev) {
+  TYCHE_USER_JWT_COOKIE_NAME = "__Host-" + TYCHE_USER_JWT_COOKIE_NAME;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-  plugins: [basicSsl(), sveltekit()],
+  plugins: [sveltekit()],
 });
